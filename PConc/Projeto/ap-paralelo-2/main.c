fb850195b3bd8fc0f6ed4a098dd6323978f3fddb
WSL_Quio <quiomachado@gmail.com>
2022-12-16 00:16:51 +0000

    update

--- PConc/Projeto/ap-paralelo-2/main.c
+++ PConc/Projeto/ap-paralelo-2/main.c
@@ -2,10 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 #include "image-lib.h"
+char WATERMARK[] = "watermark.png";
 
 int main(int argc, char **argv)
 {
+    char *img_dir_path = NULL;
+    char dir_extra[] = "./";
+    int n_img = 0;
+    char img_file_path[] = "image-list.txt";
+    FILE *img_file = NULL;
+    char img_list[40][100];
+    char buffer[100];
+    if (argc != 2)
+    {
+        printf("Wrong Number of arguments.\n");
+        printf("./ap-paralelo-1 img_dir\n");
+        exit(1);
+    }
 
+    img_dir_path = (char *)malloc(sizeof(char) * (strlen(argv[1]) + 4));
+    strcpy(img_dir_path, dir_extra);
+    strcat(img_dir_path, argv[1]);
+    strcat(img_dir_path, "/");
+
+    img_file = fopen(img_file_path, "r");
+    if (img_file == NULL)
+    {
+        printf("File %s not found.\n", img_file_path);
+        exit(1);
+    }
+    while (fscanf(img_file, "%s", buffer) == 1)
+    {
+        strcpy(img_list[n_img], img_dir_path);
+        strcat(img_list[n_img], buffer);
+        n_img++;
+    }
+
+    free(img_dir_path);
     return 0;
 }
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 10 / 3);
+    printf("%d\n", 11 / 4);
+    return 0;
+}
