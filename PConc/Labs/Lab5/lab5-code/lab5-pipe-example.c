009ab8c5ab55cf1dfb1b0fe5ccbec0bbf14ae17a
WSL_Quio <quiomachado@gmail.com>
2023-01-13 01:12:17 +0000

    up

--- PConc/Labs/Lab5/lab5-code/lab5-pipe-example.c
+++ PConc/Labs/Lab5/lab5-code/lab5-pipe-example.c
@@ -1,15 +1,31 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 /**
  * @brief program that creates a pipe, writes values to a it and reads them
  *
  */
+
+int pipe_fd[2];
+
+void *thread_function(void *arg)
+{
+    int *int_arg = (int *)arg;
+    int value = *int_arg;
+    int read_value = -1;
+    while (1)
+    {
+        read(pipe_fd[0], &read_value, sizeof(read_value));
+        printf("Thread %d just read %d from pipe_fd[0]\n", value, read_value);
+    }
+    pthread_exit(NULL);
+}
+
 int main()
 {
     // tow files descriptiors used to write. read on the pipe
-    int pipe_fd[2];
 
     // initialization of the pipe
     if (pipe(pipe_fd) != 0)
@@ -18,23 +34,25 @@ int main()
         exit(-1);
     }
     int n = 0;
-    int read_value;
+
+    pthread_t thread_id[4];
+    for (int i = 0; i < 4; i++)
+    {
+        pthread_create(&thread_id[i], NULL, thread_function, (void *)i);
+    }
+
     // infinite look that in each iteration writes a number to the pipe and read it aftwards
     while (1)
     {
         printf("going to write %d into pipe_fd[1]\n", n);
+        write(pipe_fd[1], &n, sizeof(n));
         n++;
         sleep(1);
-
-        write(pipe_fd[1], &n, sizeof(n));
         // pipe_fd[1] correpsonded to en entry of data in the pipe
         // &n is apointer to the data to be written
         // sizeof(n) is the ammount of data to write in the pipe
-
-        read(pipe_fd[0], &read_value, sizeof(read_value));
         // pipe_fd[0] correpsonded to the exit of data in the pipe
         // &n is apointer to variable that will hold the read data
         // sizeof(n) is the ammount of data to be read from the pipe
-        printf("\tjust read %d from pipe_fd[0]\n", read_value);
     }
 }
